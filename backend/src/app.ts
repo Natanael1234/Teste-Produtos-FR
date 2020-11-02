76b826c19275f8b0d07079e96041c6ee39640adc
Natanael1234 <natanaelnosorio@gmail.com>
2020-11-02 04:31:02 -0300

    Api de pedidos

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,7 @@
 import express, { NextFunction, Request, Response } from 'express';
 import { carrinhoRouter } from './routes/carrinho';
-
 import { produtoRouter } from './routes/produtos';
+import { pedidoRouter } from './routes/pedido';
 
 var cors = require('cors');
 const bodyParser = require('body-parser');
@@ -13,6 +13,7 @@ const port: number = 3000;
 
 app.use(produtoRouter);
 app.use(carrinhoRouter);
+app.use(pedidoRouter);
 
 let server = app.listen(port, () => {
     console.log(`Servidor - ouvindo na porta: ${port}`);
